4cbd5a043d7727fdcae492089d402f622692e95f
gzmyil <96871298+gzmyil@users.noreply.github.com>
2022-01-08 22:37:52 +0300

    try to commit

--- src/com/company/Bserisi.java
+++ src/com/company/Bserisi.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Bserisi extends Car{
     public void goster(){
-        System.out.println("Motor tipi ");
+        System.out.println("Motor tipi  ");
     }
 }
